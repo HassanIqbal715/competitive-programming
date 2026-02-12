69119f9c0f1d0d5c5ebea68f7898cb474e4f00bd
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2026-02-12 18:28:30 +0500

    Complete 07. Reverse Integer

---
+++ Leetcode/07-reverse-integer/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int reverse(int x) {
+    int res = 0;
+    while (x != 0) {
+        if (res > INT_MAX/10 || res < INT_MIN/10) return 0;
+        res *= 10;
+        res += (x % 10);
+        x /= 10;
+    }
+    return res;
+}
+
+int main() {
+    cout << reverse(2147483648);
+}
